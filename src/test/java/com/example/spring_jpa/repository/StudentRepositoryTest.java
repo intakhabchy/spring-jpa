5b0afeaf18cd1ba67b0510c7cb4d455750f769c9
intakhabchy <intakhab.chy@gmail.com>
2025-05-22 00:21:22 +0600

    test find all

--- src/test/java/com/example/spring_jpa/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/spring_jpa/repository/StudentRepositoryTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -25,5 +27,12 @@ class StudentRepositoryTest {
                 .build();
 
         studentRepository.save(student);
+    }
+
+    @Test
+    public void printAllStudent(){
+        List<Student> studentList = studentRepository.findAll();
+
+        System.out.println("studentList: "+studentList);
     }
 }
