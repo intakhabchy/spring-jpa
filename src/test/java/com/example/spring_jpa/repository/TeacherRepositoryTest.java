15a69383bb8e3e21a8420659c55c8f31257d290a
intakhabchy <intakhab.chy@gmail.com>
2025-06-01 01:00:52 +0600

    one to many save - teacher and course list

---
+++ src/test/java/com/example/spring_jpa/repository/TeacherRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.spring_jpa.repository;
+
+import com.example.spring_jpa.entity.Course;
+import com.example.spring_jpa.entity.Teacher;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TeacherRepositoryTest {
+
+    @Autowired
+    private TeacherRepository teacherRepository;
+
+    @Test
+    public void saveTeacher(){
+
+        Course courseOOP = Course.builder()
+                .title("OOP")
+                .credit(4)
+                .build();
+
+        Course courseDBA = Course.builder()
+                .title("DBA")
+                .credit(4)
+                .build();
+
+        Teacher teacher = Teacher.builder()
+                .firstName("T_f_name")
+                .lastName("T_l_name")
+                .courses(List.of(courseOOP,courseDBA))
+                .build();
+
+        teacherRepository.save(teacher);
+    }
+}
